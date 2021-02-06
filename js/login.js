68ad1effe0f294d287483982b452b96cd42be6e7
Alexaresmarin <78609491+Alexaresmarin@users.noreply.github.com>
2021-02-06 21:44:30 +0100

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,24 +1,17 @@
-function navSlide() {
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".nav-links");
-    const navLinks = document.querySelectorAll(".nav-links li");
-    
-    burger.addEventListener("click", () => {
-        //Toggle Nav
-        nav.classList.toggle("nav-active");
-        
-        //Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = ""
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.75}s`;
-            }
-        });
-        //Burger Animation
-        burger.classList.toggle("toggle");
-    });
-    
-}
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("#login")
+    const createAccountForm = document.querySelector("#createAccount")
 
-navSlide();
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden");
+    });
+
+    document.querySelector('#linkLogin').addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove("form--hidden");
+        createAccountForm.classList.add("form--hidden");
+    });
+
+});

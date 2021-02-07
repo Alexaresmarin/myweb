e2a7ea0cdce882c214903a805a5b74471dd4236a
Alexaresmarin <78609491+Alexaresmarin@users.noreply.github.com>
2021-02-07 14:03:26 +0100

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -18,19 +18,6 @@ function clearInputError(inputElement) {
 
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#login")
-    const createAccountForm = document.querySelector("#createAccount")
-
-    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.add("form--hidden");
-        createAccountForm.classList.remove("form--hidden");
-    });
-
-    document.querySelector('#linkLogin').addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.remove("form--hidden");
-        createAccountForm.classList.add("form--hidden");
-    });
 
     loginForm.addEventListener("submit", e => {
         e.preventDefault();
@@ -38,14 +25,4 @@ document.addEventListener("DOMContentLoaded", () => {
         setFormMessage(loginForm, "error", "Invalid username/password combination");
     });
 
-    document.querySelectorAll(".form__input").forEach(inputElement => {
-        inputElement.addEventListener("blur", e => {
-            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 5) {
-                setInputError(inputElement, "Username must be at leat 5 characters in lenght");
-            }
-        });
-        inputElement.addEventListener("input", e => {
-            clearInputError(inputElement);
-        });
-    });
 });
